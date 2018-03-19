ee0f62877a2593f1756fa83be80d5445dcedac7d
João Soares <joaotsoares@deloitte.pt>
2018-03-19 10:59:25 +0000

    Test docker script for container name

--- Docker scripts/Support/Start/01.StartConfigServer.sh
+++ Docker scripts/Support/Start/01.StartConfigServer.sh
@@ -6,12 +6,12 @@ SCRIPTPATH=`dirname "$0"`
 #Generate random uuid
 uuid=$(uuidgen)
 
-#UUID
-uuid=${uuid,,}
+#Container Name
+containerName="config-server-${uuid,,}"
 
 #Start Container
-echo "Starting config-server..."
-docker run --name config-server-${uuid} -d -p 8071:8071 --net host config-server-service:latest
+echo "Instantiating new Config-Server on container: ${containerName}"
+docker run --name ${containerName} -d -p 8071:8071 --net host config-server-service:latest
 
 #Check for service availability
 source $SCRIPTPATH/99.waitForService.sh localhost 8071 health;
