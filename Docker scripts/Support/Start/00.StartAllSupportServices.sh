f2ac7a42f998f232376c45fb4faa0573997b3e31
João Soares <joaotsoares@deloitte.pt>
2018-03-19 11:22:12 +0000

    Updated Start All Support Script

--- Docker scripts/Support/Start/00.StartAllSupportServices.sh
+++ Docker scripts/Support/Start/00.StartAllSupportServices.sh
@@ -3,9 +3,31 @@
 #Get this script folder
 SCRIPTPATH=`dirname "$0"`
 
-#Execute start scripts for each support service
+#Set Number of Instances
+EUREKAINSTACES=3
+ZUULINSTANCES=2
+
+#Start Config Server
 source $SCRIPTPATH/01.StartConfigServer.sh
-source $SCRIPTPATH/02.StartEurekaServer.sh
-source $SCRIPTPATH/03.StartZuul.sh
+
+#Start Eureka Servers
+source $SCRIPTPATH/02.1.StartFirstEurekaServer.sh
+
+if [ "$EUREKAINSTACES" -le 2]; then
+	source $SCRIPTPATH/02.2.StartSecondEurekaServer.sh
+fi
+
+if [ "$EUREKAINSTACES" -le 3]; then
+	source $SCRIPTPATH/02.3.StartThirdEurekaServer.sh
+fi
+
+#Start Zuuls
+source $SCRIPTPATH/03.1.StartFirstZuul.sh
+
+if [ "$ZUULINSTACES" -le 2]; then
+source $SCRIPTPATH/03.2.StartSecondZuul.sh
+fi
+
+#Start management services
 source $SCRIPTPATH/04.StartAdminServer.sh
 source $SCRIPTPATH/05.StartOrdinaDashboard.sh
